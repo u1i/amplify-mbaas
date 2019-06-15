fde74eccb7fc38bea62e02f2065a83b66ed1121f
Uli Hitzel <uli.hitzel@gmail.com>
2019-06-15 12:59:20 +0000

    changed

--- 05-mass-generate.sh
+++ 05-mass-generate.sh
@@ -4,8 +4,9 @@ cookie=$(cat cookies.txt | tr "\t" "\n" | tail -1)
 
 while [ 1 ]
 do
-
+	echo "$(date) generate data"
 	cat /dev/urandom | hexdump -c | head -20000 | base64 > data.txt
 	#curl -b cookies.txt -c cookies.txt -X PUT -F "name=key-$(date +%s)" -F "type=binary" -F "value=<data.txt" "https://api.cloud.appcelerator.com/v1/keyvalues/set.json?key=$appkey"
-	curl -b cookies.txt -c cookies.txt -X PUT -F "name=key-$(date +%s)" -F "value=<data.txt" "https://api.cloud.appcelerator.com/v1/keyvalues/set.json?key=$appkey"
+	echo "$(date) RUN request"
+	time curl -b cookies.txt -c cookies.txt -X PUT -F "name=ws2key-$(date +%s)" -F "value=<data.txt" "https://api.cloud.appcelerator.com/v1/keyvalues/set.json?key=$appkey"
 done
