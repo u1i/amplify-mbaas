159011b2ba912e199595897d566a2709a5e978df
Uli Hitzel <uli.hitzel@gmail.com>
2019-06-17 17:14:20 +0000

    changed

--- 05-mass-generate.sh
+++ 05-mass-generate.sh
@@ -1,7 +1,7 @@
 source appkey.conf
 
 # 20000 is ~ 2MB
-size=20000
+size=200
 runid=$RANDOM
 
 cookie=$(cat cookies.txt | tr "\t" "\n" | tail -1)
@@ -16,7 +16,6 @@ do
 
 	echo "$(date) generate data"
 	cat /dev/urandom | hexdump -c | head -$size | base64 > data-$runid.txt
-	#curl -b cookies.txt -c cookies.txt -X PUT -F "name=key-$(date +%s)" -F "type=binary" -F "value=<data.txt" "https://api.cloud.appcelerator.com/v1/keyvalues/set.json?key=$appkey"
 	echo "$(date) RUN request"
-	time curl -b cookies.txt -c cookies.txt -X PUT -F "name=ws2key-$(date +%s)" -F "value=<data-$runid.txt" "https://api.cloud.appcelerator.com/v1/keyvalues/set.json?key=$appkey"
+	time curl -b cookies.txt -c cookies.txt -X PUT -F "name=ws2key-$(date +%s)-$RANDOM" -F "value=<data-$runid.txt" "https://api.cloud.appcelerator.com/v1/keyvalues/set.json?key=$appkey"
 done
